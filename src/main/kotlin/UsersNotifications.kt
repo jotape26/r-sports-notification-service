e074de883f0bda6e75ad8565a8c5e613c5b2b484
João Leite <joao.leite@valemobi.com.br>
2019-06-11 17:53:53 -0300

    Addind pushy to make easier

--- src/main/kotlin/UsersNotifications.kt
+++ src/main/kotlin/UsersNotifications.kt
@@ -4,6 +4,15 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.firestore.DocumentReference
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
+import io.undertow.Undertow
+import io.undertow.client.UndertowClient
+import io.undertow.server.HttpHandler
+import io.undertow.util.HttpString
+import com.relayrides.pushy.apns.ApnsClientBuilder
+import com.relayrides.pushy.apns.ApnsClient.DEVELOPMENT_APNS_HOST
+import com.relayrides.pushy.apns.ApnsClient
+import java.io.File
+
 
 @Suppress("UNCHECKED_CAST")
 class UsersNotifications {
@@ -27,7 +36,26 @@ class UsersNotifications {
             val user = test["user"] as DocumentReference
             val username = user.get().get().getString("userNotificationToken")
             print(username)
+
+            val server = Undertow.builder()
+                .addHttpListener(443, "api.sandbox.push.apple.com")
+                .setHandler(HttpHandler { exchange ->
+                    exchange.requestHeaders.put(HttpString("path"), "/3/device/" + username)
+                    exchange.requestHeaders.put(HttpString("apns-push-type"), "alert")
+                    exchange.requestMethod = HttpString("POST")
+                    exchange.connection.pushResource("api.sandbox.push.apple.com", HttpString("POST"), exchange.requestHeaders)
+
+                }).build()
+
+            server.start()
+
         }
+
+        val apnsClient = ApnsClientBuilder()
+            .setClientCredentials(File("src/RSports_Push_Sandbox.p12"), "xxxx")
+            .build()
+
+//        apnsClient.sendNotification()
 
     }
 }
