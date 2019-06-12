2a08b7310cbadfd4e6b2a4bfa74cb4044542b14c
João Pedro Leite <joaopleite26@gmail.com>
2019-06-11 21:22:42 -0300

    Change flow

--- src/main/kotlin/com/jpapps/NotificationGateway.kt
+++ src/main/kotlin/com/jpapps/NotificationGateway.kt
@@ -1,5 +1,8 @@
 package com.jpapps
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.jpapps.notification.UsersNotifications
 import io.ktor.application.*
 import io.ktor.http.*
@@ -8,15 +11,27 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.io.FileInputStream
 
 fun main(args: Array<String>) {
 
     System.setProperty("Dio.netty.tryReflectionSetAccessible", "true")
+
     val port = System.getenv("PORT")?.toInt() ?: 8080
+
+    val serviceAccount = FileInputStream("src/r-sports-backend-firebase-adminsdk-t7d2b-e1908f0145.json")
+
+    val options = FirebaseOptions.Builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://r-sports-backend.firebaseio.com")
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
     embeddedServer(Netty, port) {
         routing {
             get("/") {
-                call.respondText("{\"success\" : true }", ContentType.Application.Json)
+                call.respondText("{\"success\" : true , \"method\" : \"push\"}", ContentType.Application.Json)
             }
             post("notifyUsers") {
                 val text = call.receiveText()
--- src/main/kotlin/com/jpapps/notification/UsersNotifications.kt
+++ src/main/kotlin/com/jpapps/notification/UsersNotifications.kt
@@ -1,10 +1,6 @@
 package com.jpapps.notification
 
-import java.io.FileInputStream
-import com.google.firebase.FirebaseApp
-import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.firestore.DocumentReference
-import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.MulticastMessage
@@ -13,16 +9,6 @@ import com.google.firebase.messaging.MulticastMessage
 @Suppress("UNCHECKED_CAST")
 class UsersNotifications {
     fun notifyUsers(){
-
-        val serviceAccount = FileInputStream("src/r-sports-backend-firebase-adminsdk-t7d2b-e1908f0145.json")
-
-        val options = FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .setDatabaseUrl("https://r-sports-backend.firebaseio.com")
-            .build()
-
-        FirebaseApp.initializeApp(options)
-
         val name = FirestoreClient.getFirestore().collection("reservas").document("uZychHNma1g1SgQaQ97M").get().get().data
 
         val jogs = name?.get("jogadores") as ArrayList<Any>
